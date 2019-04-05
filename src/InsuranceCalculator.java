fba5ddf328980dff4411b96ed62bde0cdbf9e19e
taneia <trreed2@bsu.edu>
2019-04-04 22:08:38 -0400

    abstract class implementation

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -7,7 +7,7 @@ public class InsuranceCalculator {
 
         if (income <= 10000) {
             strategy = new InsuranceStrategyLow();
-            return strategy.calculateInsuranceVeryHigh(income);
+            return strategy.calculate(income);
         } else if (income <= 30000) {
             strategy = new InsuranceStrategyMedium();
             return strategy.calculateInsuranceVeryHigh(income);
